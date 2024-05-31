f53ad6e49843204a768841f0462e824b32ec672b
AndresJr16 <142357313+realyty2005@users.noreply.github.com>
2024-05-31 14:07:06 -0500

    Delete Tienda_tecnologica directory

--- Tienda_tecnologica/Slider.js
+++
@@ -1,8 +0,0 @@
-document.querySelector('.carrusel').addEventListener('mouseenter', function() {
-    document.querySelector('.fotos-overlay').style.display = 'block';
-});
-
-// Ocultar el overlay al quitar el mouse del carrusel
-document.querySelector('.carrusel').addEventListener('mouseleave', function() {
-    document.querySelector('.fotos-overlay').style.display = 'none';
-});
